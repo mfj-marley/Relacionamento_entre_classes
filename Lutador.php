b5d8d44bdbf7795c1301c1907c96b3455bcb9475
mfj-marley <jesus.marley@graduacao.uerj.br>
2022-10-14 22:47:48 -0300

    0.1

--- Lutador.php
+++ Lutador.php
@@ -1,6 +1,7 @@
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title></title>
 </head>
 <body>
@@ -10,93 +11,96 @@
             private $nome,$nacionalidade,$idade,$altura,$peso,$categoria,$vitorias,$derrotas,$empates;
 
             //Metodos especiais
-            public function getNome()
-            {
+            public function __construct($no, $na, $id, $al, $pe, $vi, $de, $em) {
+                $this->nome = $no;
+                $this->nacionalidade = $na;
+                $this->idade = $id;
+                $this->altura = $al;
+                $this->peso = $pe;
+                $this->vitorias = $vi;
+                $this->derrotas = $de;
+                $this->empates = $em;
+            }
+            public function getNome() {
                 return $this->nome;
             }
-            public function setNome($nome)
-            {
+            public function setNome($nome) {
                 $this->nome = $nome;
             }
-            public function getNacionalidade()
-            {
+            public function getNacionalidade() {
                 return $this->nacionalidade;
             }
-            public function setNacionalidade($nacionalidade)
-            {
+            public function setNacionalidade($nacionalidade) {
                 $this->nacionalidade = $nacionalidade;
             }
-            public function getIdade()
-            {
+            public function getIdade() {
                 return $this->idade;
             }
-            public function setIdade($idade)
-            {
+            public function setIdade($idade) {
                 $this->idade = $idade;
             }
-            public function getAltura()
-            {
+            public function getAltura() {
                 return $this->altura;
             }
-            public function setAltura($altura)
-            {
+            public function setAltura($altura) {
                 $this->altura = $altura;
             }
-            public function getPeso()
-            {
+            public function getPeso() {
                 return $this->peso;
             }
-            public function setPeso($peso)
-            {
-                $this->peso = $peso;
+            public function setPeso($pe) {
+                $this->peso = $pe;
+                $this->setCategoria();
             }
-            public function getCategoria()
-            {
+            public function getCategoria() {
                 return $this->categoria;
             }
-            public function setCategoria($categoria)
-            {
-                $this->categoria = $categoria;
+            public function setCategoria() {
+                if ($this->peso < 52.2) {
+                    $this->categoria = "Inválido";
+                }elseif ($this->peso <= 70.3) {
+                    $this->categoria = "Leve";
+                }elseif ($this->peso <= 83.9) {
+                    $this->categoria = "Médio";
+                }elseif ($this->peso <= 120.2) {
+                    $this->categoria = "Pesado";
+                }else {
+                    $this->categoria = "Inválido";
+                }
             }
-            public function getVitorias()
-            {
+            public function getVitorias() {
                 return $this->vitorias;
             }
-            public function setVitorias($vitorias)
-            {
+            public function setVitorias($vitorias) {
                 $this->vitorias = $vitorias;
             }
-            public function getDerrotas()
-            {
+            public function getDerrotas() {
                 return $this->derrotas;
             }
-            public function setDerrotas($derrotas)
-            {
+            public function setDerrotas($derrotas) {
                 $this->derrotas = $derrotas;
             }
-            public function getEmpates()
-            {
+            public function getEmpates() {
                 return $this->empates;
             }
-            public function setEmpates($empates)
-            {
+            public function setEmpates($empates) {
                 $this->empates = $empates;
             }
             //Metodos
-            public function apresentar(){
+            public function apresentar() {
 
             }
-            public function status(){
+            public function status() {
 
             }
-            public function ganharLuta(){
-
+            public function ganharLuta() {
+                $this->setVitorias($this->getVitorias() + 1);
             }
-            public function perderLuta(){
-
+            public function perderLuta() {
+                $this->setDerrotas($this->getDerrotas() +1);
             }
-            public function empatarLuta(){
-
+            public function empatarLuta() {
+                $this->setEmpates($this->getEmpates() +1);
             }
         }
     ?>
